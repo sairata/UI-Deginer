6d993704973343319d74b8102fca0932df3c40ec
Sairata Ahladini Rout <sairataghosh@gmail.com>
2019-05-28 10:53:34 +0530

    textfield

---
+++ src/TextField.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+
+class TextField extends React.Component {
+    
+    render(){
+        return(
+            <div>
+              <input type="text" className="styleTextfield" />
+            </div>
+        )
+    }
+}
+
+export default TextField;
